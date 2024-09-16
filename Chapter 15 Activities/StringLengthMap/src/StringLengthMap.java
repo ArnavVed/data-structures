50dda4730b17f067d5a13601b0cb0c9d490e8a32
Arnav R Vedavyas <ARVedavyas@stu.naperville203.org>
2024-09-16 14:07:36 -0500

    stringlenthmap

--- Chapter 15 Activities/StringLengthMap/src/StringLengthMap.java
+++ Chapter 15 Activities/StringLengthMap/src/StringLengthMap.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.io.*;
+
 /**
  * Read all words from a file and add them to a
  * map whose keys are word lengths and whose values
@@ -16,42 +17,55 @@ public class StringLengthMap
 
         try (Scanner in = new Scanner(new File(filename)))
         {
-
-           Map<Integer, String> map = new HashMap<>();
-            
+            // Create a map with word length as the key and comma-separated strings as values
+            Map<Integer, String> map = new HashMap<>();
 
             while (in.hasNext())
             {
                 String word = clean(in.next());
                 Integer len = word.length();
 
-                // Update the map here
-                // Modify Worked Example 15.1
-                
-
-
+                // Update the map without using merge()
+                if (map.containsKey(len)) {
+                    // If the key already exists, append the word to the existing string
+                    String existingWords = map.get(len);
+                    map.put(len, existingWords + ", " + word);
+                } else {
+                    // If the key doesn't exist, create a new entry
+                    map.put(len, word);
+                }
             }
 
-            // Print the strings, in increasing order of their length
-            // Use this format: 1: i, a, i
+            // Sort the map by the length of the words (keys)
+            List<Integer> sortedKeys = new ArrayList<>(map.keySet());
+            Collections.sort(sortedKeys);
+
+            // Print the map in the format: length: word1, word2, word3
+            for (Integer key : sortedKeys)
+            {
+                System.out.println(key + ": " + map.get(key));
+            }
+
         } catch (FileNotFoundException e)
         {
             System.out.println("Cannot open: " + filename);
         }
     }
 
+    /**
+     * Clean the word by removing non-letter characters and converting to lowercase.
+     */
     public static String clean(String s)
     {
-        String r = "";
+        StringBuilder r = new StringBuilder();
         for (int i = 0; i < s.length(); i++)
         {
             char c = s.charAt(i);
             if (Character.isLetter(c))
             {
-                r = r + c;
+                r.append(c);
             }
         }
-        return r.toLowerCase();
+        return r.toString().toLowerCase();
     }
-
 }
