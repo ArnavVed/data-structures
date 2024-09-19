95fdd59046ea0eca1b9b1acc9648fab0bec2b7ce
ArnavVed <arvedavyas@stu.naperville203.org>
2024-09-19 13:41:14 -0500

    d

--- Chapter 15 Class Notes/src/QueueDemo.java
+++ Chapter 15 Class Notes/src/QueueDemo.java
@@ -9,5 +9,26 @@ public class QueueDemo
 {
     public static void main(String[] args)
     {
+        //Create a print queue of strings using a linked list
+        Queue<String> jobs = new LinkedList<>();
+
+        //add some print jobs
+
+        jobs.add("Joes Quarter 2 expense report");
+        jobs.add("sallays secret mnecraft world");
+        
+        System.out.println("Printing: " + jobs.remove());
+
+        jobs.add("Kathy really top secret document");
+        jobs.add("Joes grocery list");
+        jobs.add("Kathy: Can I get fired for this");
+
+        System.out.println("Printing: " + jobs.remove());
+
+        jobs.add("Boss: kathys termination letter");
+
+        while (jobs.size() > 0){
+            System.out.println("Printing: " + jobs.remove());
+        }
     }
 }
